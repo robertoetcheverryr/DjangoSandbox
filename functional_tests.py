cc99946e362617a0724d718747083cb107a6ba4b
robertoetcheverryr <yggdrasil.raiker@gmail.com>
2020-05-10 22:03:49 -0300

    Functional test now tests we can input a to do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 
@@ -17,16 +19,28 @@ class NewVisitorTest(unittest.TestCase):
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
-        # She is invited to enter a to-do item straight away
         # User is invited to enter a to-do item right away
-
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
         # Enter a task and when hitting enter the pages refreshes
         # and lists the task and a number in an itemized list
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
         # There is still a text box to add tasks
-
+        self.fail('Finish the test!')
         # Adding another task refreshes the page and shows both
 
         # The page mentions that to access this specific list in the future
