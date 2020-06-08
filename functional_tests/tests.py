c57708c32d68d3fecc67d75751fb6d3d91f88364
robertoetcheverryr <yggdrasil.raiker@gmail.com>
2020-06-07 22:03:17 -0300

    Add bootstrap static files and implement template inheritance for home and list

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,6 +1,6 @@
 # Django test files MUST begin with tests and the classes themselves must also start with Test
 
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
@@ -9,7 +9,7 @@ import time
 MAX_WAIT = 10
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
