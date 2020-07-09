54127833d000ed0e4740557f241c96f9e0abc1be
robertoetcheverryr <yggdrasil.raiker@gmail.com>
2020-07-08 23:42:13 -0300

    Minor fixes to Docker configs.

--- Docker/PushImagesToDockerHub.cmd
+++ Docker/PushImagesToDockerHub.cmd
@@ -1,3 +1,3 @@
 docker push etcheverryroberto/djangosandbox
 docker push etcheverryroberto/nginx-static
-docker push etcheverryroberto/nginx-ssl-loadbalancer
+docker push etcheverryroberto/nginx-ssl-load-balancer
